0d47f434b6fcdfbbd10324f6be11a5522ebdf084
Briant Guzman <BriantGuzman@gmail.com>
2019-12-04 11:16:12 -0500

    LoopExample.java

---
+++ LoopExample.java
@@ -0,0 +1,23 @@
+
+
+public class LoopExample { 
+
+
+  public static void main(String[] args)
+  { 
+    
+    for(int i = 0; i < 10; i++)
+    {
+      System.out.println(i);
+    }
+    
+    String[] arr = new String[4];
+    for(char a : arr)
+    {
+      System.out.println(a);
+    }
+    
+  }
+
+
+}
